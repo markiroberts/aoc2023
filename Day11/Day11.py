dad1a49b93624c1fed7afc239f91fb60acfd4bc9
Mark <markiroberts@yahoo.com>
2023-12-16 07:02:54 +0000

    main

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -14,6 +14,10 @@ class itemlist:
         self.list = []
 
     def additem(self, item):
+        if item not in self.list:
+            self.list.append(item)
+
+    def addsorteditem(self, item):
         a = item[0]
         b = item[1]
         if a > b:
@@ -23,6 +27,24 @@ class itemlist:
         if item not in self.list:
             self.list.append(item)
 
+def rowEmpty(row=0):
+ 
+    boEmpty = True
+    for universe in universelist.list:
+        if universe[1] == row:
+            boEmpty = False
+            break
+    return boEmpty
+
+def columnEmpty(col=0):
+ 
+    boEmpty = True
+    for universe in universelist.list:
+        if universe[0] == col:
+            boEmpty = False
+            break
+    return boEmpty
+    
 
 filename = './Day11/example11.txt'
 file = open(filename,'r', encoding='utf8').read().split('\n')    
@@ -43,25 +65,31 @@ universelist = itemlist()
 
 for y in range(height):
     line = array[y]
-    print (line)
     for x in range(len(line)):
         if line[x] == "#":
             pairitem = [x,y]
             universelist.additem(pairitem)
 
+for y in range(height):
+    print (array[y], rowEmpty(y))
+
 print (universelist.list)
 
-mylist = itemlist()
-pairitem = [1,2]
-mylist.additem(pairitem)
-pairitem = [3,3]
-mylist.additem(pairitem)
-pairitem = [3,4]
-mylist.additem(pairitem)
-pairitem = [4,3]
-mylist.additem(pairitem)
-pairitem = [3,43123]
-mylist.additem(pairitem)
-print (mylist.list)
+for y in reversed(range(height)):
+    if (rowEmpty(y)):
+        for universe in universelist.list:
+            if universe[1] > y:
+                universe[1] = universe[1] + 1
 
+print (universelist.list)
+
+for x in reversed(range(width)):
+    print(x,columnEmpty(x))
+
+for x in reversed(range(width)):
+    if (columnEmpty(x)):
+        for universe in universelist.list:
+            if universe[0] > x:
+                universe[0] = universe[0] + 1
+print (universelist.list)
 
